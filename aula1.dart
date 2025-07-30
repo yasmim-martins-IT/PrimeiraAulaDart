7e79d4ea5296730900ef336c4794101639c503cc
Yasmim Sampaio Martins <53661815857@>
2025-07-30 15:45:26 -0300

    primeira aula e primeiro commit

---
+++ aula1.dart
@@ -0,0 +1,6 @@
+import 'dart:io'; //importando as funções da linguagem dart
+
+/* comentario em mais de uma linha    */
+
+//criando a void main para rodar a aplicação obrigatório em TODO arquivo
+void main() {}
